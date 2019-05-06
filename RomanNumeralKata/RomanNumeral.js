55bdb54e41e23fb7cc905a277fe5487a4f809558
Pete <petergarvin1@gmail.com>
2019-05-06 21:27:27 +0100

    solved for 1, 2, 3

--- RomanNumeralKata/RomanNumeral.js
+++ RomanNumeralKata/RomanNumeral.js
@@ -1,5 +1,8 @@
 exports.Converter = decimal => {
 
-return;
+    return decimal == 1 ? "I"
+          :decimal == 2 ? "II"
+          :"III";
+
 
 }
--- RomanNumeralKata/__tests__/RomanNumeralKata.test.js
+++ RomanNumeralKata/__tests__/RomanNumeralKata.test.js
@@ -1,10 +1,12 @@
-const RomanNumeral = require("../RomanNumeralKata.js");
+const RomanNumeral = require("../RomanNumeral.js");
 require("jest-each");
 
 describe("RomanNumeralKata", () => {
   test.each`
-    input                                  | expectedResult
-    ${1} | ${"I"}
+    input | expectedResult
+    ${1}  | ${"I"}
+    ${2}  | ${"II"}
+    ${3}  | ${"III"}
 
   `(
     "should return numeral value of $expectedResult given $input",
