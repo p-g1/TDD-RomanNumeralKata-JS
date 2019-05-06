00429ad3c229bf96838d8d1ef2ea68834282b547
Pete <petergarvin1@gmail.com>
2019-05-06 22:18:14 +0100

    refactored

--- RomanNumeralKata/RomanNumeral.js
+++ RomanNumeralKata/RomanNumeral.js
@@ -1,7 +1,12 @@
-exports.Converter = decimal => {
-    const decimalToRoman = 
-            {5:"V", 4:"IV", 3:"III", 2:"II", 1:"I"}
-    return decimalToRoman[decimal];
+exports.Converter = number => {
+    const map = {V:5,IV:4,I:1};
+    var output = '';
 
-
+    for (var numeral in map ) {
+        while ( number >= map[numeral] ) {
+            output += numeral;
+            number -= map[numeral];
+        }
+    }
+    return output;
 }
